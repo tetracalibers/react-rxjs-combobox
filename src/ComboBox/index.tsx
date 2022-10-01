3b5a2a5ba20722289d03930c723ba5ab3800d3cb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-02 00:19:58 +0900

    Rootコンポーネントをファイル内で宣言

--- src/ComboBox/index.tsx
+++ src/ComboBox/index.tsx
@@ -1,7 +1,17 @@
 import _ from "lodash"
 import { ChoiceItem } from "./types/ChoiceItem"
-import { Root } from "./Root"
 import { AutoComplete } from "./AutoComplete"
+import styled from "styled-components"
+
+const Root = styled.div`
+  position: relative;
+  display: block;
+  width: 200px;
+  height: 50vh;
+  margin: 0 auto;
+  font-size: 18px;
+  color: #60666d;
+`
 
 type ComboBoxProps = {
   choices: ChoiceItem[]
